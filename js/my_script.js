6bd1d81d13e6993f4bf2c5feb3b4e0bee6601f0d
dkkabc1234 <106858983+dkkabc1234@users.noreply.github.com>
2022-07-13 19:56:43 +0700

    Add files via upload

---
+++ js/my_script.js
@@ -0,0 +1,17 @@
+﻿function noImage(imgage){
+	imgage.onerror="";
+	imgage.src="images/commont/noimage.png";
+}
+
+$(window).ready(function(){
+	$('.my_tooltip').each(function(){
+		var this_width=$(this).outerWidth();
+
+		var this_margin=(-this_width/2)+($(this).parent().outerWidth()/2);
+		$(this).css({'margin-left':this_margin});
+	})
+
+	$('.item_tooltip').hover(function(){
+		$('.my_tooltip').fadeToggle();
+	});
+});
